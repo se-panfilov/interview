ac5da730688d5cdd16953f1fde51d47458b5ee59
Sergey Panfilov <se-panfilov@ya.ru>
2017-03-06 21:30:45 +0800

    added factorial

--- js/carring.js
+++ js/carring.js
@@ -1,3 +1,4 @@
+//1. My
 function sum (a) {
   const wrap = function (b) {
     const c = a + b
@@ -11,5 +12,47 @@ function sum (a) {
   return wrap
 }
 
-sum(2)(5)(10)
+let result
 
+result = sum(2)(5)(10)
+console.info(result)
+
+//2. My, reduce // TODO (S.Panfilov)
+function sum2(cur, arr) {
+  arr = arr || []
+  arr.push (cur)
+
+  let result = arr.reduce((v,p) => v+p)
+
+  function wrap (val) {
+    return sum(val, arr)
+  }
+
+  wrap.valueOf = function () {
+    console.info(result)
+  }
+
+  return wrap
+}
+
+result = sum2(2)(5)(10)
+console.info(result)
+
+//3. Original
+function sum3 (a) {
+  let currentSum = a
+
+  function wrap (b) {
+    currentSum += b
+    return wrap
+  }
+
+  wrap.toString = function () {
+    return currentSum
+  }
+
+  return wrap
+}
+
+// result = sum3(2)(5)(10)
+// console.info(result)
