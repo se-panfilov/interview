07ca9adec4abcb0e88d48911d33e1bebc673d8fb
Sergei Panfilov <sergei.panfilov@powerhouse.nl>
2018-09-27 09:50:10 +0200

    added questions

---
+++ ask.js
@@ -0,0 +1,30 @@
+// Questions
+// 1. About your self
+// 2. Do you have any open sourcing projects?
+// 3. How about unit tests?
+// 4. Functional style, immutability, immutable js?
+// 5. Experience with frameworks? Rxjs?
+// 6. typescript?
+// 7. IDE
+// 8. Agile, Srum?
+// 9. OS? Windows/Linux
+// 10. Yur last project and team
+
+// ---------------------------------------------------------------------
+
+// 1. FuzzBuzz
+
+//  function should be called like fussBuzz([1, 3, 5, 4, 4, 7, 9, 15, 25, 30])
+// if number can be divided by 3 - return 'Fuzz'
+// if number can be divided by 5 - return 'Buz'
+// if both - 'FuzzBuzz'
+
+// Bonus - can you get rid of "for"
+
+// 2. Phone formatting
+// console.info(solution('004-448-555-583-61')) //004-448-555-583-61
+// console.info(solution('0 - 22 1985--324')) //022-198-532-4
+// console.info(solution('004-448-555-583-61')) //555-372-654
+// console.info(solution('004-448-555-583-613-344')) //555-372-654
+
+
