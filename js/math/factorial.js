63029d19ca183057dedc440d9357e5220a2155c0
Sergei <se-panfilov@ya.ru>
2018-12-17 09:22:01 +0100

    add

--- js/math/factorial.js
+++ js/math/factorial.js
@@ -1,13 +1,15 @@
 //1. recursive
-function fact (n, result = []) {
+function fact(n, result = []) {
   if (n <= 0) return result
 
   result.push(n)
   return fact(n - 1, result)
 }
 
+console.info(fact(5))
+
 //2. simple
-function fact2 (n) {
+function fact2(n) {
   const result = []
 
   for (let i = n; i > 0; i--) {
@@ -17,9 +19,14 @@ function fact2 (n) {
   return result
 }
 
-let result
-result = fact(5)
-console.info(result)
+console.info(fact2(5))
 
 // result = fact2(5)
 // console.info(result)
+
+//2. simple & functional
+function fact3(n) {
+  return new Array(n).fill(undefined).map((v, i) => n - i, n)
+}
+
+console.info(fact3(5))
