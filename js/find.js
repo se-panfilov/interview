712393442414ef6a66646b0428baf2b8853fa3ca
Sergei Panfilov <se-panfilov@ya.ru>
2019-08-06 15:01:19 +0200

    asdsad

--- js/find.js
+++ js/find.js
@@ -1,6 +1,10 @@
-function find (elem, arr, position = 0) {
+const list = ['a', 'b', 'c', 'd']
+
+function find(elem, arr, position = 0) {
   if (position >= arr.length) return -1
   if (elem === arr[position]) return position
 
   return find(elem, arr, position + 1)
 }
+
+console.info(find('c', list));
